3b6b0f08247f9ee33a5801e2e58fd1f3db05cc17
Stephen McConnel <stephen_mcconnel@sil.org>
2021-03-04 13:35:29 -0700

    Create Harvester youtrack issues for New or Updated Books (BL-8919)
    This sidesteps the current limit of 5 error reports per day maximum.
    There is now a limit of 1 report per new/updated book per day, and
    5 reports per uploader of new/updated books per day, so there is still
    some limits on bug reports per day, but not as strict for new (and
    updated) books.

--- src/Harvester/AlertManager.cs
+++ src/Harvester/AlertManager.cs
@@ -3,21 +3,30 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BloomHarvester.Parse.Model;
 
 namespace BloomHarvester
 {
 	/// <summary>
-	/// This class keeps track of alerts that are reported and whether alerts should be silenced for being too "noisy" (frequent)	///
-	/// Use the TryReportAlerts(out bool isSilenced) function to do this
+	/// This class keeps track of alerts that are reported and whether alerts should be silenced for being too "noisy" (frequent)
+	/// Use the RecordAlertAndCheckIfSilenced() function to do this
 	/// </summary>
 	public class AlertManager
 	{
 		internal const int kMaxAlertCount = 5;
 		const int kLookbackWindowInHours = 24;
 
+		internal class Alert
+		{
+			internal DateTime TimeStamp;
+			internal string BookUrl;
+			internal string HarvestState;
+			internal string UploaderId;
+		}
+
 		private AlertManager()
 		{
-			_alertTimes = new LinkedList<DateTime>();
+			_alerts = new LinkedList<Alert>();
 		}
 
 		// Singleton access
@@ -36,7 +45,7 @@ namespace BloomHarvester
 		}
 
 		// Other fields/properties
-		private LinkedList<DateTime> _alertTimes;	// This list should be maintained in ascending order
+		private LinkedList<Alert> _alerts;	// This list should be maintained in ascending time order
 		internal IMonitorLogger Logger { get; set; }
 
 		// Methods
@@ -45,11 +54,18 @@ namespace BloomHarvester
 		/// Tells the AlertManager to record that an alert is taking place. Also checks if the alert should be silenced or not
 		/// </summary>
 		/// <returns>Returns true if the current alert should be silenced, false otherwise</returns>
-		public bool RecordAlertAndCheckIfSilenced()
+		public bool RecordAlertAndCheckIfSilenced(BookModel bookModel = null)
 		{
-			_alertTimes.AddLast(DateTime.Now);
+			var timeStamp = new Alert()
+			{
+				TimeStamp = DateTime.Now,
+				BookUrl = bookModel?.BaseUrl,
+				HarvestState = bookModel?.HarvestState,
+				UploaderId = bookModel?.Uploader.ObjectId,
+			};
+			_alerts.AddLast(timeStamp);
 
-			bool isSilenced = this.IsSilenced();
+			bool isSilenced = this.IsSilenced(bookModel);
 			if (isSilenced && Logger != null)
 			{
 				Logger.TrackEvent("AlertManager: An alert was silenced (too many alerts).");
@@ -63,21 +79,35 @@ namespace BloomHarvester
 		/// </summary>
 		public void Reset()
 		{
-			_alertTimes.Clear();
+			_alerts.Clear();
 		}
 
 		/// <summary>
 		/// Returns whether alerts should currently be silenced
 		/// </summary>
 		/// <returns>Returns true for silenced, false for not silenced</returns>
-		private bool IsSilenced()
+		private bool IsSilenced(BookModel bookModel = null)
 		{
 			// Determine how many alerts have been fired since the start time of the lookback period
 			PruneList();
 
+			// Allow reports for "New" or "Updated" books even after exceeding the daily quota for bug
+			// reports in general.  But limit to one report per book or MAX reports per uploader.  (Of
+			// course, if the uploader persists with one book MAX times, no reports will appear for other
+			// books that get uploaded later the same day.)
+			// See https://issues.bloomlibrary.org/youtrack/issue/BL-8919.
+			if (bookModel?.HarvestState == "New" || bookModel?.HarvestState == "Updated")
+			{
+				var countForThisBook = _alerts.Count(stamp => stamp?.BookUrl == bookModel.BaseUrl);
+				if (countForThisBook > 1)
+					return true;		// silence if this book has been reported already today
+				var countForUploader = _alerts.Count(stamp => stamp?.UploaderId == bookModel.Uploader.ObjectId);
+				return countForUploader > kMaxAlertCount;	// silence for too many errors caused by books from the same uploader.
+			}
+
 			// Current model has the same (well, inverted) condition for entering and exiting the Silenced state.
 			// Another model could use unrelated conditions for entering vs. exiting the Silenced state
-			return _alertTimes.Count > kMaxAlertCount;
+			return _alerts.Count > kMaxAlertCount;
 		}
 
 		/// <summary>
@@ -88,9 +118,9 @@ namespace BloomHarvester
 			DateTime startTime = DateTime.Now.Subtract(TimeSpan.FromHours(kLookbackWindowInHours));
 
 			// Precondition: This list must be in sorted order
-			while (_alertTimes.Any() && _alertTimes.First.Value < startTime)
+			while (_alerts.Any() && _alerts.First.Value.TimeStamp < startTime)
 			{
-				_alertTimes.RemoveFirst();
+				_alerts.RemoveFirst();
 			}
 		}
 	}
